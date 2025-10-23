c799eefcfe4b5e7e2c5369a7f1e83657f251e0e9
SavichArtem <mods_b@mail.ru>
2025-10-23 21:38:17 +0300

    feat: add read.js stream

---
+++ src/streams/read.js
@@ -0,0 +1,27 @@
+/* node read.js ../fs/freelancers_data/freelancers_index.json (нахожусь в папке streams)*/
+
+const fs = require('fs');
+const path = require('path');
+
+const readFileStream = (filename) => {
+  const readStream = fs.createReadStream(filename, { encoding: 'utf8' });
+
+  readStream
+    .on('data', (chunk) => {
+      console.log(chunk);
+    })
+    .on('error', (err) => {
+      console.error(`Ошибка чтения: ${err.message}`);
+    })
+    .on('end', () => {
+      console.log('Чтение файла завершено');
+    });
+};
+
+const [, , filename] = process.argv;
+if (!filename) {
+  console.error('Использование: node read.js <filename>');
+  process.exit(1);
+}
+
+readFileStream(filename);
